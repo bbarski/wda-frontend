fd83eb2e277283452c4f6b20001c7acd589885d2
bbarski <bartosz.barski@mail.com>
2020-01-25 18:38:22 +0100

    proto to work with

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {WeatherDataModel} from "./model/WeatherDataModel";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component<any, any> {
+
+    state: State = { weatherData: null };
+    apiUrl:string="http://51.38.131.247:8080/?";
+    defaultCity:string="midmar";
+
+    componentDidMount(){ this.gatherData(this.defaultCity); }
+
+    render() { return this.displayData() }
+
+    displayData = (): any => {
+
+        if (this.state.weatherData == null) {
+            return (<div className="dimmer ui active ">
+                        <div className="ui loader">
+                        </div>
+                    </div>)
+        }
+
+        return (
+            <div id="weather-data" className="ui raised very padded text container ui center aligned">
+                <div className="ui segments center aligned">
+                    <h1 className="ui center aligned header">{this.state.weatherData.stationName}</h1>
+                    {
+                        Object.values(this.state.weatherData).map((value: any, key: any) => {
+                            return <div className="ui segment center aligned compact blue">{value}</div>;})
+                    }
+                    <select id="pick-city" onChange={this.grabValue}>
+                        <option value="select city">select city</option>
+                        <option value="midmar">midmar</option>
+                        <option value="gosport">gosport</option>
+                    </select>
+                </div>
+
+            </div>
+        )
+    };
+
+    grabValue = (event: any): any =>{ this.gatherData(event.target.value);};
+
+    gatherData = (paramValue: any): any =>{
+
+        this.setState({weatherData: null});
+
+        let url = new URL(this.apiUrl);
+        let params = {cityName:paramValue};
+        url.search = new URLSearchParams("cityName=".concat(params.cityName.valueOf())).toString();
+
+        fetch(url.toString())
+            .then(res => (res.json()))
+            .then((data) => { this.setState({weatherData: data});
+                console.log("weatherDataState is: " + this.state.weatherData)})
+            .catch(console.log)
+    };
+
 }
 
-export default App;
+    interface State { weatherData: null | WeatherDataModel }
---
+++ src/model/WeatherDataModel.ts
@@ -0,0 +1,19 @@
+export class WeatherDataModel {
+
+    constructor() {}
+
+    avgSpeedKnots: string;
+    barometerHPa: string;
+    dateLabel: string;
+    dayTime: string;
+    hourTime: string;
+    minuteTime: string;
+    monthTime: string;
+    outsideHumidityPercent: string;
+    outsideTempCelsius: string;
+    secondsTime: string;
+    stationName: string;
+    weatherDesc: string;
+    windDirCompass: string;
+
+}
