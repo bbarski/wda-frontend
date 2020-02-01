9bda7a1bd3c336e0620c877a15eeb5ffd47f9b2c
bbarski <bartosz.barski@mail.com>
2020-02-01 14:52:00 +0100

    proto to work with

--- src/Server.ts
+++ src/Server.ts
@@ -4,7 +4,7 @@ export const path = require('path');
 
 export const app = express();
 
-app.use(express.static(path.join(__dirname, '/build')));
-app.get('/', function(req:any, res:any) {res.sendFile(path.join(__dirname, '/build', 'index.html'));});
+app.use(express.static(path.join(__dirname, '../build')));
+app.get('/', function(req:any, res:any) {res.sendFile(path.join(__dirname, '../build', 'index.html'));});
 
 app.listen(9000);
